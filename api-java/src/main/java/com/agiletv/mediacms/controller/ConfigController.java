e35ebd19feaa9031c0a378de74ea7ec422888aea
David Martin Romero <davidmartinromero75@gmail.com>
2026-05-27 14:19:45 +0200

    added ConfigController.java

--- api-java/src/main/java/com/agiletv/mediacms/controller/ConfigController.java
+++ api-java/src/main/java/com/agiletv/mediacms/controller/ConfigController.java
@@ -1,10 +1,40 @@
 package com.agiletv.mediacms.controller;
 
+import com.agiletv.mediacms.config.AppConfig;
+import com.agiletv.mediacms.dto.ConfigResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/config")
 public class ConfigController {
+    private final AppConfig appConfig;
 
+    public ConfigController(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
+
+    @GetMapping
+    public ResponseEntity<ConfigResponse> getConfig(
+            @RequestParam(defaultValue = "consumer") String clientType) {
+        AppConfig.ClientConfig client = appConfig.getClients().get(clientType.toLowerCase());
+        if (clientType == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(new ConfigResponse(
+                clientType.toUpperCase(),
+                new ConfigResponse.Theme(
+                        client.getTheme().getPrimaryColor(),
+                        client.getTheme().getSecondaryColor()
+                        ),
+                new ConfigResponse.Features(
+                        client.getFeatures().isDownloadEnabled(),
+                        client.getFeatures().getMaxConcurrentStreams(),
+                        client.getFeatures().getAllowedFormats(),
+                        client.getFeatures().getAllowedDrm()
+                )
+        ));
+    }
 }
