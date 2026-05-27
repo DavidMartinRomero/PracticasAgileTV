8f29ec4aa1f0fc4e1d7458e4f07fc2b244d4071e
David Martin Romero <davidmartinromero75@gmail.com>
2026-05-27 12:45:35 +0200

    Added AppConfig.java & ConfigResponse.java

---
+++ api-java/src/main/java/com/agiletv/mediacms/dto/ConfigResponse.java
@@ -0,0 +1,15 @@
+package main.java.com.agiletv.mediacms.dto;
+
+import java.util.List;
+
+public record ConfigResponse(String clientType, Theme theme, Features features) {
+
+    public record Theme(String primaryColor, String secondaryColor){}
+
+    public record Features(
+            boolean downloadEnabled,
+            int maxConcurrentsStreams,
+            List<String> allowedFormats,
+            List<String> allowedDrm
+    ) {}
+}
